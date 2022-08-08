5059f0f75010ab454e5080b07d08f4bd7fd6bef5
Cristina Gomez <g0m3z.c@gmail.com>
2022-08-07 19:04:32 -0600

    update

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,41 @@
+-- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+-- Retrieve the title, from_date, and to_date columns from the Titles table.
+-- Create a new table using the INTO clause.
+-- Join both tables on the primary key.
+-- Filter the data on the birth_date column to retrieve the employees who were born between 
+-- 1952 and 1955. Then, order by the employee number.
+-- Export the Retirement Titles table from the previous step as retirement_titles.csv and save 
+-- it to your Data folder in the Pewlett-Hackard-Analysis folder.
+-- Export the table.
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+--Write another query to retrieve the number of employees by most recent title about to retire.
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+select * from unique_titles;
